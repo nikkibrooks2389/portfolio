f44bceb5fba4695682584fd054c64191675d1888
nikkibrooks2389 <nbrooks2389@gmail.com>
2023-08-18 15:40:31 -0400

    create skills section on about me page

--- src/components/views/About.styles.js
+++ src/components/views/About.styles.js
@@ -22,32 +22,24 @@ const AboutContainer = styled.div`
 `;
 
 const AboutMeSection = styled.div`
-  width:70%
-  align-self:center;
   animation: ${slideInFromRightFadeIn} 0.7s ease forwards;
-  opacity: 0;
 `;
 
 const AboutMeHeader = styled.h1`
   font-size: 3.5rem;
-  margin: 0 0 1rem 0;
+  margin-bottom:1rem;
   animation: ${slideInFromRightFadeIn} 0.9s ease forwards;
-  opacity: 0;
 `;
 
 const AboutMeDescription = styled.p`
+padding-top:.5rem;
   color:${({ theme }) => theme.colors.secondaryText};
-  font-size: 1.1rem;
-  margin: 0 0 2rem 0;
+  font-size: 1.2rem;
   font-family: ${({ theme }) => theme.fonts.secondary}
 `;
 
 const ImageGridContainer = styled.div`
   display:flex
-  width: 30%; 
-  padding-right:1rem;
-  opacity: 0; // Start hidden
-  transform: translateY(20px); 
   animation: ${slideUpFadeIn} 0.5s forwards; 
   animation-delay: 1.2s; 
 
@@ -57,11 +49,65 @@ const ImageGridContainer = styled.div`
   }
 `;
 
+const SkillsContainer = styled.div`
+  display:flex
+  flex-direction:column;
+  min-width: 200px; 
+  padding-right:1rem;
+  // opacity: 0; // Start hidden
+  // transform: translateY(20px); 
+  // animation: ${slideUpFadeIn} 0.5s forwards; 
+  // animation-delay: 1.2s; 
+
+  // @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+  //   width: 100%; // Full width on smaller screens
+  //   padding: 0; // No padding on smaller screens
+  // }
+`;
+
+const SkillSection = styled.div`
+  display:flex;
+  flex-direction:column;
+
+  // padding-right:1rem;
+  // opacity: 0; // Start hidden
+  // transform: translateY(20px); 
+  // animation: ${slideUpFadeIn} 0.5s forwards; 
+  // animation-delay: 1.2s; 
+
+  // @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+  //   width: 100%; // Full width on smaller screens
+  //   padding: 0; // No padding on smaller screens
+  // }
+`;
+
+const SkillAboutContainer = styled.div`
+  display:flex;
+  flex-direction:row;
+ margin-top:1.5rem;
+  // padding-right:1rem;
+  // opacity: 0; // Start hidden
+  // transform: translateY(20px); 
+  // animation: ${slideUpFadeIn} 0.5s forwards; 
+  // animation-delay: 1.2s; 
+
+  // @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+  //   width: 100%; // Full width on smaller screens
+  //   padding: 0; // No padding on smaller screens
+  // }
+`;
+
+const Skill = styled.span`
+font-family : ${({ theme }) => theme.fonts.secondary};
+color: ${({ theme }) => theme.colors.secondaryText};
+padding:.3rem
+
+`
+
+
 const ContactMe = styled(Link)`
 text-decoration:none;
 color:${({ theme }) => theme.colors.secondaryAccent};
-
-
 `
 
 
@@ -78,22 +124,57 @@ const About = () => {
     <ViewsWrapper>
 
       <AboutContainer>
+        <AboutMeSection>
+          <AboutMeHeader>About Me.</AboutMeHeader>
+          <Resume />
+          <SkillAboutContainer>
+
+            <SkillsContainer>
+              <SkillSection>
+                <h2>Frontend Tools</h2>
+                <Skill>JavaScript(ES6+)</Skill>
+                <Skill>HTML5</Skill>
+                <Skill>CSS3/SCSS</Skill>
+                <Skill>React</Skill>
+                <Skill>React Native</Skill>
+                <Skill>Redux/Redux Toolkit</Skill>
+                <Skill>Bootstrap</Skill>
+                <Skill>MUI / MUI Base</Skill>
+                <Skill>Antd</Skill>
+                <Skill>Styled Components</Skill>
+                <Skill>AmCharts</Skill>
+              </SkillSection>
+
+              <SkillSection>
+                <h2 style={{ marginTop: "1rem" }}>Backend Tools</h2>
+                <Skill>Node.js</Skill>
+                <Skill>Express</Skill>
+
+              </SkillSection>
+              <SkillSection>
+                <h2 style={{ marginTop: "1rem" }}>DevOps</h2>
+                <Skill>Git</Skill>
+                <Skill>GitHub</Skill>
+                <Skill>Bitbucket</Skill>
+                <Skill>Docker</Skill>
+                <Skill>Aws</Skill>
+              </SkillSection>
+
+            </SkillsContainer>
+            <AboutMeDescription>
+              Hailing from Massachusetts and now thriving in sunny San Diego, California, I'm a frontend developer with a design flair and an innovative spirit.<br /><br />
+              After graduating from Learn Academy's coding bootcamp in San Diego, I further refined my skills with an internship at Victorise, immersing myself in React Native. This experience paved the way for my most recent role at iCompaas. Here, I took the lead on projects for brands like Oneloan Direct and Wannapractice, using tools such as React, Redux, Node.js, and CSS, while integrating Docker and AWS to enhance our workflow.<br /><br />
+
+              A rapid learner with a strong work ethic, I flourish in collaborative environments, always eager to both share knowledge and learn from peers. Away from the digital canvas, I treasure moments with my dogs, skateboarding San Diego's picturesque routes, and soaking up its beautiful beaches.<br /><br />
+
+              I'm actively exploring new frontend opportunities, eager to bring my expertise and passion to dynamic teams. If you'd like to connect or discuss potential collaborations, please feel free to reach out to me through my <ContactMe to={"/contact"}>Contact Page</ContactMe>
+            </AboutMeDescription>
+
+          </SkillAboutContainer>
+        </AboutMeSection>
         <ImageGridContainer>
           <ImageGrid images={images} />
         </ImageGridContainer>
-        <AboutMeSection>
-          <AboutMeHeader>About Me.</AboutMeHeader>
-          <AboutMeDescription>
-            Hailing from Massachusetts and now thriving in sunny San Diego, California, I'm a frontend developer with a design flair and an innovative spirit.<br /><br />
-            After graduating from Learn Academy's coding bootcamp in San Diego, I further refined my skills with an internship at Victorise, immersing myself in React Native. This experience paved the way for my most recent role at iCompaas. Here, I took the lead on projects for brands like Oneloan Direct and Wannapractice, using tools such as React, Redux, Node.js, and CSS, while integrating Docker and AWS to enhance our workflow.<br /><br />
-
-            A rapid learner with a strong work ethic, I flourish in collaborative environments, always eager to both share knowledge and learn from peers. Away from the digital canvas, I treasure moments with my dogs, skateboarding San Diego's picturesque routes, and soaking up its beautiful beaches.<br /><br />
-
-            I'm actively exploring new frontend opportunities, eager to bring my expertise and passion to dynamic teams. If you'd like to connect or discuss potential collaborations, please feel free to reach out to me through my <ContactMe to={"/contact"}>Contact Page</ContactMe>
-          </AboutMeDescription>
-
-          <Resume />
-        </AboutMeSection>
       </AboutContainer>
     </ViewsWrapper>
   )
--- src/components/views/ViewsWrapper.js
+++ src/components/views/ViewsWrapper.js
@@ -5,8 +5,9 @@ export const ViewsWrapper = styled.div`
   display:flex;
   justify-content:center;
   min-height:100vh;
-  padding: 90px 45px 20px 45px;
-  background: ${({ theme }) => theme.colors.primaryBackground};   color: ${props => props.theme.colors.primaryText};
+  padding: 90px 20px 20px 20px;
+  background: ${({ theme }) => theme.colors.primaryBackground};
+  color: ${props => props.theme.colors.primaryText};
   animation: ${gradientAnimation} 15s infinite,
   animation-delay: 0.7s;
 
